b87aadf03039c796e866c90d29af1d3fe824a3f0
Faddaoui jihen <jihen20faddaoui>
2020-12-18 21:33:55 +0100

    FirstProject-Native

---
+++ Screens/Screen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View , Stylesheet , Text } from 'react-native' ;
+
+
+ const Screen =()=> {
+    
+    return (
+
+       
+
+        <View>
+<View>
+<Text>     Home Screen Hello  ........  </Text>
+</View>
+<StatusBar style="auto"/>
+</View>
+
+     
+    )
+}
+
+
+export default Screen;
